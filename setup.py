8fdbf625cc18f330a077bdd9df573ec4b563dcca
Surenbayar <suugii.sd@gmail.com>
2022-05-13 15:33:31 +0800

    Fix: update setup py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="verify4py",
-    version="0.1.0",
+    version="0.1.1",
     author="Surenbayar Doloonjin",
     author_email="surenbayar@ailab.mn",
     description="Issue certificates using blockchain and smart contract",
